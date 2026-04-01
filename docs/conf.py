08bd4ba234729f9ee90104d1ae61705aa87ffd95
bkiyota <brettckiyota@gmail.com>
2026-04-01 15:10:46 +0900

    readthedocs documentation

---
+++ docs/conf.py
@@ -0,0 +1,36 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'milk'
+copyright = '2026, Brett Kiyota'
+author = 'Brett Kiyota'
+release = '0.1.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+extensions = [
+    "myst_parser",  # enables Markdown support
+    'sphinx_rtd_theme'
+]
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "sphinx_rtd_theme"
+html_static_path = ['_static']
+html_theme_options = {
+    'navigation_depth': 4,    # how many levels to show in sidebar
+    'collapse_navigation': False,
+    'sticky_navigation': True
+}
